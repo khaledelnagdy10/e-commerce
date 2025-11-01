9ffdd7ba8a46ece059802b74c00531ccd2a799c5
khaledelnagdy10 <128099437+khaledelnagdy10@users.noreply.github.com>
2025-11-01 14:49:23 +0200

    add remove function to bag cubit

--- lib/features/presentation/bag/features/controller/Bag cubit/bag_cubit.dart
+++ lib/features/presentation/bag/features/controller/Bag cubit/bag_cubit.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:equatable/equatable.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:store_app2/core/utils/models/all_product_model.dart';
@@ -15,7 +16,30 @@ class BagCubit extends Cubit<BagState> {
     emit(BagUpdated(bagList: List.from(bagProductsList)));
   }
 
+  void removeFromBagList(AllProductModel product) {
+    if (isAddedToCart(product)) {
+      bagProductsList.remove(product);
+    }
+    emit(BagUpdated(bagList: List.from(bagProductsList)));
+  }
+
   bool isAddedToCart(AllProductModel product) {
     return bagProductsList.any((p) => p.id == product.id);
+  }
+
+  void incrementNumberProduct(AllProductModel product) {
+    final index = bagProductsList.indexWhere((p) => p.id == product.id);
+    if (index != -1) {
+      bagProductsList[index].quantity++;
+      emit(BagUpdated(bagList: List.from(bagProductsList)));
+    }
+  }
+
+  void decrementNumberProduct(AllProductModel product) {
+    final index = bagProductsList.indexWhere((p) => p.id == product.id);
+    if (index != -1 && bagProductsList[index].quantity > 1) {
+      bagProductsList[index].quantity--;
+      emit(BagUpdated(bagList: List.from(bagProductsList)));
+    }
   }
 }
