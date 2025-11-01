fb9bf9153b69fb4187e42e83fbac546aa9f6c712
khaledelnagdy10 <128099437+khaledelnagdy10@users.noreply.github.com>
2025-11-01 21:01:50 +0200

    edit profile view

--- lib/features/presentation/profile/view/profile_view.dart
+++ lib/features/presentation/profile/view/profile_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:store_app2/features/presentation/profile/widgets/profile_info_body.dart';
+import 'package:store_app2/core/utils/text_style.dart';
+import 'package:store_app2/features/presentation/profile/widgets/personal_details_info_body.dart';
 
 class ProfileView extends StatelessWidget {
   const ProfileView({super.key});
@@ -9,7 +10,69 @@ class ProfileView extends StatelessWidget {
     return Scaffold(
       body: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 15.0, vertical: 60),
-        child: ProfileInfoBody(),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text('My Profile', style: Style.textStyleBoldHeadLine),
+
+            SizedBox(height: 20),
+            Row(
+              children: [
+                Expanded(
+                  child: ListTile(
+                    title: Text('My Orders'),
+                    subtitle: Text(
+                      'Check your orders',
+                      style: Style.textStyle11grey,
+                    ),
+                  ),
+                ),
+                IconButton(
+                  onPressed: () {},
+                  icon: Icon(
+                    Icons.arrow_forward_ios,
+                    size: 17,
+                    color: Colors.grey.shade600,
+                  ),
+                ),
+              ],
+            ),
+            Divider(color: Colors.grey.shade300),
+            SizedBox(height: 20),
+            Row(
+              children: [
+                Expanded(
+                  child: InkWell(
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) {
+                            return PersonalDetailsInfoBody();
+                          },
+                        ),
+                      );
+                    },
+                    child: ListTile(
+                      title: Text('Personal details'),
+                      subtitle: Text(
+                        'Email,Password',
+                        style: Style.textStyle11grey,
+                      ),
+                    ),
+                  ),
+                ),
+
+                Icon(
+                  Icons.arrow_forward_ios,
+                  size: 17,
+                  color: Colors.grey.shade600,
+                ),
+              ],
+            ),
+            Divider(color: Colors.grey.shade300),
+          ],
+        ),
       ),
     );
   }
