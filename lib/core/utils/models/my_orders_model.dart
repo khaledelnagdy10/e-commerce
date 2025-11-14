51709fbbf9d6fd248e6b70332ed5a392ec713d2d
khaledelnagdy10 <128099437+khaledelnagdy10@users.noreply.github.com>
2025-11-14 16:00:02 +0200

    add id & status to MyOrderModel

--- lib/core/utils/models/my_orders_model.dart
+++ lib/core/utils/models/my_orders_model.dart
@@ -1,11 +1,16 @@
 import 'package:store_app2/core/utils/models/all_product_model.dart';
 
 class MyOrdersModel {
+  String? id;
   final List<AllProductModel> products;
-
   final DateTime createdAt;
+  String status;
 
-  MyOrdersModel({required this.products, required this.createdAt});
+  MyOrdersModel({
+    required this.products,
+    required this.createdAt,
+    this.status = 'Pending',
+  });
 
   factory MyOrdersModel.fromJson(json) {
     return MyOrdersModel(
@@ -13,12 +18,15 @@ class MyOrdersModel {
           .map((p) => AllProductModel.fromJson(p))
           .toList(),
       createdAt: DateTime.parse(json['createdAt']),
+      status: json['status'] ?? 'Pending',
     );
   }
+
   Map<String, dynamic> toJson() {
     return {
       'products': products.map((p) => p.toJson()).toList(),
       'createdAt': createdAt.toIso8601String(),
+      'status': status,
     };
   }
 }
