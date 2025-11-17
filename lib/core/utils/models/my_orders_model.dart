2af1291b0542da009fb1b106d2fb40b4e2c09ca6
khaledelnagdy10 <128099437+khaledelnagdy10@users.noreply.github.com>
2025-11-17 18:39:13 +0200

    add address to submit_order_view

--- lib/core/utils/models/my_orders_model.dart
+++ lib/core/utils/models/my_orders_model.dart
@@ -1,12 +1,16 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:store_app2/core/utils/models/all_product_model.dart';
 
 class MyOrdersModel {
   String? id;
+  String? address;
   final List<AllProductModel> products;
   final DateTime createdAt;
   String status;
 
   MyOrdersModel({
+    this.id,
+    required this.address,
     required this.products,
     required this.createdAt,
     this.status = 'Pending',
@@ -19,6 +23,7 @@ class MyOrdersModel {
           .toList(),
       createdAt: DateTime.parse(json['createdAt']),
       status: json['status'] ?? 'Pending',
+      address: json['address'] ?? '',
     );
   }
 
@@ -27,6 +32,7 @@ class MyOrdersModel {
       'products': products.map((p) => p.toJson()).toList(),
       'createdAt': createdAt.toIso8601String(),
       'status': status,
+      'address': address,
     };
   }
 }
